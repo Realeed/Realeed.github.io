a008c175521dfe624851c70c7d769c9f1a18676d
Realeed <hsargsyan395@gmail.com>
2021-05-24 02:41:44 +0400

    m

--- node/index.js
+++ node/index.js
@@ -2,10 +2,10 @@ const express = require('express');
 const app = express();
 const port = 5000;
 
-app.listen(port, () => console.log(`Listening to port: ${port}`));
-
 app.use(express.static('../'));
 
 app.get('', (req, res) => {
     res.sendFile(__dirname + '../index.html');
 })
+
+app.listen(port, () => console.log(`Listening to port: ${port}`));
