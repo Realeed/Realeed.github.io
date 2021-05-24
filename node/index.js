6959cad49060b76c060635684134dbc35d598a3a
Realeed <hsargsyan395@gmail.com>
2021-05-25 03:42:43 +0400

    m

--- node/index.js
+++ node/index.js
@@ -1,11 +1,31 @@
-const express = require('express');
-const app = express();
-const port = 80;
+const express = require('express')
+const app = express()
+const port = 80
 
-app.use(express.static('../'));
-
-app.get('', (req, res) => {
-    res.sendFile(__dirname + '../index.html');
+app.use(express.static('../'))
+app.get('/admin', (req, res) => {
+    res.sendFile('C:/Soft/caseShop/Admin/admin.html')
+})
+app.get(('/iphone'), (req, res) => {
+    res.sendFile('C:/Soft/caseShop/iPhone/iphone.html')
+})
+app.get('/samsung', (req, res) => {
+    res.sendFile('C:/Soft/caseShop/Samsung/samsung.html')
+})
+app.get('/airpod', (req, res) => {
+    res.sendFile('C:/Soft/caseShop/Airpod/airpod.html')
+})
+app.get('/custom', (req, res) => {
+    res.sendFile('C:/Soft/caseShop/Custom/custom.html')
+})
+app.get('/support', (req, res) => {
+    res.sendFile('C:/Soft/caseShop/Support/support.html')
+})
+app.get('/airpod', (req, res) => {
+    res.sendFile('C:/Soft/caseShop/Airpod/airpod.html')
+})
+app.get('/covid', (req, res) => {
+    res.sendFile('C:/Soft/caseShop/Covid/covid.html')
 })
 
-app.listen(port, () => console.log(`Listening to port: ${port}`));
+app.listen(port, () => console.log(`Listening to port: ${port}`))
