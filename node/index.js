6882351cde1ee61a87a7a0a7377e3bea8f50065c
Realeed <hsargsyan395@gmail.com>
2021-05-25 19:02:59 +0400

    m

--- node/index.js
+++ node/index.js
@@ -8,14 +8,8 @@ const Passcode = require('./caseShopdb/mongodb')
 const { send } = require('process')
 
 const dbURI = 'mongodb+srv://realeed:covid.3355@caseshop.e4xmp.mongodb.net/caseShop?retryWrites=true&w=majority'
-mongoose.connect(dbURI, { useNewUrlParser: true,  useUnifiedTopology: true })
-    .then(result => {
-        console.log('Connected to db')
-        app.listen(port, () => console.log(`Listening to port... ${port}`))
-    })
-    .catch(err => console.log(err))
 
-const urlencodedParser = bodyParser.urlencoded({extended: false})
+const urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 app.use(express.static('../'))
 app.use(express.static('../Admin'))
@@ -46,29 +40,24 @@ app.get('/admin', (req, res) => {
     res.sendFile('C:/Soft/caseShop/Admin/passcode.html')
 })
 app.post('/addproduct', urlencodedParser, (req, res) => {
-    const testdb = new Passcode(
-        {
-            passcode: crypto.createHash('sha256').update(req.body.passcode).digest('hex')
-        }
-    )
-    testdb.save()
-        .then(result => res.send('Passcode is set'))
+    mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+        .then(result => {
+            console.log('Connected to db')
+            let hash = crypto.createHash('sha256').update(req.body.passcode).digest('hex')
+            Passcode.find()
+            .then(result => {
+                const storedHash = result[0].passcode
+                if (hash == storedHash) {
+                    res.sendFile('C:/Soft/caseShop/Admin/addproduct.html')
+                } else {
+                    res.send('Incorrect passcode!')
+                }
+            })
+            .catch(err => console.log(err))
+        })
         .catch(err => console.log(err))
-    // let hash = crypto.createHash('sha256').update(req.body.passcode).digest('hex')
-    // Passcode.find()
-    // .then(result => {
-    //     const storedHash = result[0].passcode
-    //     if (hash == storedHash) {
-    //         res.sendFile('C:/Soft/caseShop/Admin/addproduct.html')
-    //     } else {
-    //         res.send('Incorrect passcode!')
-    //     }
-    // })
-    // .catch(err => console.log(err))
 })
 
-app.get('/setthepasscode', (req, res) => {
-
-})
+app.listen(port, () => console.log(`Listening to port... ${port}`))
 
 
